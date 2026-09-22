be8c80b0bd6727e5a1e2cc10d7a663f18307e123
dimitar0811 <160605749+dimitar0811@users.noreply.github.com>
2026-09-22 14:56:39 +0300

    Clean up DashboardPage formatting

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -9,27 +9,28 @@ export class DashboardPage {
 
   constructor(page: Page) {
     this.page = page;
+
     this.heading = this.page.getByRole('heading', { name: 'Dashboard' });
     this.welcomeMessage = this.page.getByText('Welcome to your dashboard.');
     this.logoutButton = this.page.getByRole('button', { name: 'Logout' });
   }
 
- async expectDashboardVisible() {
-  await expect(
-    this.page.getByRole('heading', { name: 'Dashboard' })
-  ).toBeVisible({ timeout: 1000 });
-}
+  async expectDashboardVisible() {
+    await expect(this.heading).toBeVisible({ timeout: 1000 });
+  }
 
- async expectWelcomeMessageVisible() {
-  await expect(this.welcomeMessage).toBeVisible();
-   
-}
-async expectLogoutButtonVisible() {
-  await expect(this.logoutButton).toBeVisible();
-}
-async logout() {
-  await this.logoutButton.click();
-}
+  async expectWelcomeMessageVisible() {
+    await expect(this.welcomeMessage).toBeVisible();
+  }
+
+  async expectLogoutButtonVisible() {
+    await expect(this.logoutButton).toBeVisible();
+  }
+
+  async logout() {
+    await this.logoutButton.click();
+  }
+
   async expectDashboardHidden() {
     await expect(this.heading).toBeHidden();
   }
